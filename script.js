8596007a2993bdd86b2e463cc0e06105dd85d57f
ishaq-siddiqui <siddiquiishaq7860@gmail.com>
2025-04-02 17:01:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let viewCount = localStorage.getItem("pageViews") || 0;
-    viewCount++;
-    localStorage.setItem("pageViews", viewCount);
-    document.getElementById("viewCount").textContent = viewCount;
-});
+
